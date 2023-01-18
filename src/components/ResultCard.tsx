3b98f66a5e655007033675dd92b4f800a66d71df
GigiDev7 <gigivadachkoria@gmail.com>
2023-01-18 16:43:12 +0400

    added more styling and components

---
+++ src/components/ResultCard.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import image from "/home.webp";
+import { AiFillStar, AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
+
+const ResultCard = () => {
+  return (
+    <div className="flex flex-col  w-1/3 lg:w-1/5 relative icon-container">
+      <div className="absolute top-1/2 -translate-y-1/2 flex justify-between w-full px-2">
+        <BsArrowLeftCircle className="text-white text-2xl cursor-pointer arrow-icon" />
+        <BsArrowRightCircle className="text-white text-2xl cursor-pointer arrow-icon" />
+      </div>
+      <AiOutlineHeart className="absolute right-2 top-2 text-2xl" />
+
+      <img className="rounded-lg" src={image} />
+      <div className="mt-2 flex justify-between">
+        <div>
+          <h2 className="font-semibold ">Tbilisi, Georgia</h2>
+          <p>
+            <span className="font-semibold">$300</span> night
+          </p>
+        </div>
+        <div>
+          <p className="flex items-center">
+            <AiFillStar /> <span>5.0</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResultCard;
--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useRef } from "react";
-import image from "/home.webp";
-import { AiFillStar } from "react-icons/ai";
+import React, { useEffect } from "react";
+import ResultCard from "../components/ResultCard";
 
 const SearchResults: React.FC = () => {
-  const elRef = useRef<any>(null);
-
   useEffect(() => {
     const handler = () => {
       const maxHeight = document.body.offsetHeight - window.innerHeight;
@@ -22,71 +19,8 @@ const SearchResults: React.FC = () => {
   }, []);
 
   return (
-    <div
-      ref={elRef}
-      className="flex flex-wrap justify-center w-full gap-12 lg:gap-7 pb-12"
-    >
-      <div className="flex flex-col  w-1/3 lg:w-1/5">
-        <img className="rounded-lg" src={image} />
-        <div className="mt-2 flex justify-between">
-          <div>
-            <h2 className="font-semibold ">Tbilisi, Georgia</h2>
-            <p>
-              <span className="font-semibold">$300</span> night
-            </p>
-          </div>
-          <div>
-            <p className="flex items-center">
-              <AiFillStar /> <span>5.0</span>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-center w-1/3 lg:w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/3 lg:w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/3 lg:w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
-      <div className="flex justify-center w-1/5">
-        <img className="" src={image} />
-      </div>
+    <div className="flex flex-wrap justify-center w-full gap-12 mt-12 lg:gap-7 pb-12">
+      <ResultCard />
     </div>
   );
 };
